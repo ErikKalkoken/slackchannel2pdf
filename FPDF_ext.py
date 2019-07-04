1625064693c69410fdca83254101c21dd5dd7ae4
Erik Kalkoken <erik.kalkoken@gmail.com>
2019-07-04 21:36:59 +0200

    improved code readability and added comments

--- FPDF_ext.py
+++ FPDF_ext.py
@@ -1,6 +1,20 @@
+# Copyright (C) 2019 Erik Kalkoken
+#
+# This package extends the FPDF class with the functionality to use 
+# rudimentary HTML for defining text formatting
+#
+# It is a port from the example in Tutorial 6 from 
+# the official FPDF documentation (http://www.fpdf.org/)
+# with some minor changes to improve readability and extensibility
+#
+# It's build upon this pyfpdf variant: https://github.com/alexanderankin/pyfpdf
+
 from fpdf import FPDF
 import re
 
+""" 
+This class extends FDPF to add the new method write_html()
+"""
 class FDPF_ext(FPDF):
     
     def __init__(self, orientation='P', unit='mm', format='A4'):        
@@ -12,48 +26,54 @@ class FDPF_ext(FPDF):
         self._href = ""
             
 
-    def write_html(self, height, html):        
+    def write_html(self, height, html):
+        """ write text with HTML tags for formatting  """
         html = html.replace("\n"," ")
+        
+        # split html into parts to identify all HTML tags
+        # even numbered parts will contain text
+        # odd numbered parts will contain tags
         parts = re.split(r'<([^>]*)>', html)
         
+        # run through all parts one by one
         for i, part in dict(enumerate(parts)).items():
             if i % 2 == 0:
-                # even parts contain text
+                # we have text
                 if len(self._href) > 0:
                     self._put_link(self._href, height, part)
                 else:
                     self.write(height, part)
 
             else:
-                # odd parts contain a tag
-                if part[0] == "/":
+                # we have a tag
+                if part[0] == "/":                    
                     self._close_tag(part[1:len(part)].upper())
                 else:
-                    # extract attributes if any
+                    # extract all attributes from the current tag if any
                     tag_parts = part.split(" ")
                     tag = tag_parts.pop(0).upper()
-                    attr = dict()                    
-                    for v in tag_parts:
-                        attr_list = re.findall(r'([^=]*)=["\']?([^"\']*)', v)
-                        if len(attr_list) > 0:
-                            for x in attr_list:
-                                attr[x[0].upper()] = x[1]
+                    attributes = dict()                    
+                    for tag_part in tag_parts:
+                        matchObj = re.search(r'([^=]*)=["\']?([^"\']*)', tag_part)
+                        if len(matchObj.groups()) == 2:
+                            attributes[matchObj.group(1).upper()] = matchObj.group(2)
+                            
                     
-                    self._open_tag(tag, attr)
+                    self._open_tag(tag, attributes)
             
 
-    def _open_tag(self, tag, attr):
-        # opening tag
+    def _open_tag(self, tag, attributes):
+        """ set style for opening tags and singular tags  """
         if tag == "B" or tag == "I" or tag == "U":
             self._set_style(tag, True)
         if tag == "A":
-            self._href = attr["HREF"]
+            self._href = attributes["HREF"]
         if tag == "BR":
             self.ln(5)
 
     
     def _close_tag(self, tag):
-        # closing tag
+        """ set style for closing tags  """
         if tag == "B" or tag == "I" or tag == "U":
             self._set_style(tag, False)
         if tag == "A":
@@ -61,6 +81,7 @@ class FDPF_ext(FPDF):
 
 
     def _set_style(self, tag, enable):
+        """ set the actual font style based on input  """
         self._tags[tag] += 1 if enable else -1
         style = ""
         for s in ["B", "I", "U"]:
@@ -70,6 +91,7 @@ class FDPF_ext(FPDF):
         self.set_font(self.font_family, size=self.font_size_pt, style=style)
 
     def _put_link(self, url, height, txt):
+        """ set style and write text to create a link  """
         self.set_text_color(0, 0, 255)
         self._set_style("U", True)
         self.write(height, txt, url)
@@ -79,7 +101,7 @@ class FDPF_ext(FPDF):
 
 def main():
     
-    html = """You can now easily print text mixing different styles: <b>bold</b>, <i>italic</i>, <u>underlined</u>, or <b><i><u>all at once</u></i></b>!<br><br>You can also insert links on text, such as <a href="http://www.fpdf.org">www.fpdf.org</a>, or on an image: click on the logo."""
+    html = """You can now easily print text mixing different styles: <b>bold</b>, <i>italic</i>, <u>underlined</u>, or <b><i><u>all at once</u></i></b>!<br><br>You can also insert links on text, such as <a href="http://www.fpdf.org" blank="_target">www.fpdf.org</a>, or on an image: click on the logo."""
     
     document = FDPF_ext()
     document.add_page()
